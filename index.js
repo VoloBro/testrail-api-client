21d6daa4783af391cbe92ab09abc08ac5d85c011
Volodymyr Prysiazhniuk <volodymyr.prysiazhniuk@nuance.com>
2020-04-02 23:30:51 -0400

    updated client default init

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./dist/testrail').TestRailClient;
+const options = {
+    domain: process.env.TESTRAIL_DOMAIN,
+    username: process.env.TESTRAIL_USERNAME,
+    password: process.env.TESTRAIL_APIKEY,
+    projectId: process.env.TESTRAIL_PROJECTID
+};
+
+const Client = require('./dist/testrail').TestRailClient;
+const outClient = new Client(options);
+
+module.exports = outClient;
